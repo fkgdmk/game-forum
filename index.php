633e71d9140ae73326302b1fe59dde1c32ae3737
Fredrik <fredrik0301@gmail.com>
2019-05-21 21:23:19 +0200

    2

--- index.php
+++ index.php
@@ -36,17 +36,6 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://www.google.com/recaptcha/api.js?render=YOUR_RECAPTCHA_SITE_KEY"></script>
-    <script>
-        grecaptcha.ready(function() {
-            grecaptcha.execute('6Lchw6MUAAAAAHzJSG5Jl6qqZ_YShb5xIF7lLXg9', {
-                action: 'contact'
-            }).then(function(token) {
-                var recaptchaResponse = document.getElementById('recaptchaResponse');
-                recaptchaResponse.value = token;
-            });
-        });
-    </script>
     <link rel="stylesheet" href="style/login.css">
     <title>Login page</title>
 </head>
@@ -54,25 +43,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 <body>
     <div class="container">
         <h3 class="title">Login</h3>
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
 
-            // Build POST request:
-            $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-            $recaptcha_secret = '6Lchw6MUAAAAAIGlEY9xttbji-Zb7Fc1odtMK2Sz';
-            $recaptcha_response = $_POST['recaptcha_response'];
-
-            // Make and decode POST request:
-            $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
-            $recaptcha = json_decode($recaptcha);
-
-            // Take action based on the score returned:
-            echo $recaptcha->score;
-
-            if ($recaptcha->score >= 0.5) { } else {
-                // Not verified - show form error
-            }
-        } ?>
         <form method="POST">
             <div class="form-group">
                 <label>E-mail</label>
